443df14c0ea69ea71417edaaad2b6763aa680d72
clonediff <32036592+clonediff@users.noreply.github.com>
2024-12-09 09:42:06 +0300

    v1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using Prometheus;
+
+const string Version = "v1",
+    VersionLabel = "Version";
+
+Counter RequestsTotal = Metrics.CreateCounter("requests_total", "Total received requests count", VersionLabel);
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () =>
+{
+    RequestsTotal.WithLabels(Version).Inc();
+    return "Hello World!";
+});
+
+app.MapMetrics();
+
+app.Run();
