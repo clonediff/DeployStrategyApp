2855b0415fe529a08ffde47d71f11e52d13c97f2
clonediff <32036592+clonediff@users.noreply.github.com>
2024-12-09 15:05:54 +0300

    fix labels

--- Program.cs
+++ Program.cs
@@ -1,16 +1,19 @@
 using Prometheus;
 
-const string Version = "v2",
+const string Version = "v1",
     VersionLabel = "Version";
 
-Counter RequestsTotal = Metrics.CreateCounter("requests_total", "Total received requests count", VersionLabel);
+const string InstanceIdLabel = "InstanceId";
+string InstanceId = Guid.NewGuid().ToString();
+
+Counter RequestsTotal = Metrics.CreateCounter("requests_total", "Total received requests count", VersionLabel, InstanceIdLabel);
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () =>
 {
-    RequestsTotal.WithLabels(Version).Inc();
+    RequestsTotal.WithLabels(Version, InstanceId).Inc();
     return "Hello World!";
 });
 
